446b36c40b3364e113c25b851dc59092dc85d901
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-06-29 14:59:17 +0400

    add views folder

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const http = require("http");
+const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const app = express();
+
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-
-const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -14,7 +14,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
