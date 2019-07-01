c3519c79854160411c03222a34c98aefd83dcb58
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-07-01 11:03:06 +0400

    add cart model

--- models/product.js
+++ models/product.js
@@ -12,7 +12,7 @@ const getProductsFromFile = cb => {
     if (err) {
       cb([]);
     } else {
-      cb(JSON.parse(JSON.stringify(fileContent)));
+      cb(JSON.parse(fileContent));
     }
   });
 };
@@ -26,6 +26,7 @@ module.exports = class Product {
   }
 
   save() {
+    this.id = Math.random().toString();
     getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
@@ -36,5 +37,12 @@ module.exports = class Product {
 
   static fetchAll(cb) {
     getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+      cb(product);
+    });
   }
 };
